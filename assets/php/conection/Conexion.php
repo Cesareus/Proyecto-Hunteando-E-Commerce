e83882f9bf4571d4015fc9bca5b9a73aca77d881
kevinmakbel <kevinmakbel75@gmail.com>
2022-11-18 05:15:23 -0300

    conectando a base de datos externa añadiendo config.json a .gitignore

--- assets/php/conection/Conexion.php
+++ assets/php/conection/Conexion.php
@@ -1,13 +1,22 @@
 <?php
+	$file = file_get_contents("config.json",true); #recupero la informacion del config.json
+	$config = json_decode($file,true); #se decodifica para asignarlo a las variables
 
-
+	
 	class Conexion{
-		private $host = "localhost"; // en pc naty "localhost:3310"
-		private $dbname = "e_commerce";
-		private $user = "cesar";
-		private $password = "cesar";
+		private $host;  // en pc naty "localhost:3310"
+		private $dbname;
+		private $user;
+		private $password;
 		private $conexion = null;
 
+		public function __construct()
+		{
+			$this->host=$GLOBALS["config"["host"]];
+			$this->dbname=$GLOBALS["config"["db_name"]];
+			$this->user=$GLOBALS["config"["username"]];
+			$this->password=$GLOBALS["config"["password"]];
+		}
 		public function getConexion(){
 			try{
 				$this->conexion = new PDO(
@@ -20,6 +29,7 @@
 				echo $e->getMessage();
 			}finally{
 				$this->conexion = null;
+				
 			}
 		}
 	}
