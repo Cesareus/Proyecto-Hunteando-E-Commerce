0d4d087876337913f0c27907748db1ea66673b72
cesareus <cesarrosa2000@gmail.com>
2022-11-21 11:15:15 -0300

    probando ajax con http

---
+++ assets/php/controllersPHP/guardar.php
@@ -0,0 +1,26 @@
+<?php
+    include '../conection/Conexion.php'; 
+
+    $producto = $_POST["array"];
+    var_dump($producto[0]);
+
+    $conexion = new Conexion();
+    $cnn = $conexion->getConexion();
+    $sql = "INSERT INTO productos(categoria, dir_imagen, titulo, descripcion, precio) VALUES (?,?,?,?,?)";
+    $statement = $cnn->prepare($sql);
+    $respuesta = false;
+
+    $statement->bindParam(1,$producto[0], PDO::PARAM_STR);
+    $statement->bindParam(2,$producto[1], PDO::PARAM_STR);
+    $statement->bindParam(3,$producto[2], PDO::PARAM_STR);
+    $statement->bindParam(4,$producto[3], PDO::PARAM_STR);
+    $statement->bindParam(5,$producto[4], PDO::PARAM_INT);
+    $respuesta = $statement->execute();
+
+    echo $respuesta;
+
+
+    
+   
+  
+ 
