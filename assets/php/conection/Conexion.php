6a93e125aa3f750870155b0db92e50828648eacc
cesareus <cesarrosa2000@gmail.com>
2022-11-25 10:36:51 -0300

    modificando gitignore y conexion

---
+++ assets/php/conection/Conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+
+	class Conexion{
+		private $host = "localhost"; // en pc naty "localhost:3310"
+		private $dbname = "e_commerce";
+		private $user = "cesar";
+		private $password = "cesar";
+		private $conexion = null;
+
+		public function getConexion(){
+			try{
+				$this->conexion = new PDO(
+									"mysql:host=$this->host; dbname=$this->dbname",
+									$this->user,
+									$this->password
+									);
+			return $this->conexion;
+			}catch(Exception $e){
+				echo $e->getMessage();
+			}finally{
+				$this->conexion = null;
+			}
+		}
+	}
--- assets/php/controllersproductos/modificar.php
+++ assets/php/controllersproductos/modificar.php
@@ -6,7 +6,7 @@ $conexion = new Conexion();
 
 try {    
     $cnn = $conexion->getConexion();
-    $sql = "UPDATE productos SET categoria=:categoria, subcategoria=:subcategoria, dir_imagen=:dir_imagen, titulo=:titulo, descripcion=:descripcion, precio=:precio WHERE id=:id";
+    $sql = "UPDATE productos SET categoria=:categoria, subcategoria=:subcategoria, dir_imagen=:dir_imagen, titulo=:titulo, descripcion=:descripcion, precio=:precio WHERE ID=:id";
     $statement = $cnn->prepare($sql);
     $statement->bindParam(":id", $data['id'], PDO::PARAM_INT);
     $statement->bindParam(":categoria", $data['categoria'], PDO::PARAM_STR);
